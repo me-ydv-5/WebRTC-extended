cd0a991ab2ff851824b07bbdac3459c27d3e9168
Sahil <yadavsahil5198@gmail.com>
2018-12-18 12:52:09 +0530

    Added navbar

--- src/components/App-new.jsx
+++ src/components/App-new.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
-import {Button} from 'semantic-ui-react'
-import ClientVideoFeed from './ClientVideoFeed'
+import {audioFile} from "../helpers/helper";
 import OthersVideoFeed from './OthersVideoFeed'
+import ReactTooltip from 'react-tooltip'
+
+// Images for the battery
+import zero from '../assets/images/0.png';
+import tfive from '../assets/images/25.png';
+import fifty from '../assets/images/50.png';
+import sfive from '../assets/images/75.png';
+import cent from '../assets/images/100.png';
 
 import '../assets/css/app.css'
 import '../assets/css/styles.css'
 import '../assets/css/canvas.css'
-import {AGORA_API_KEY} from "../constants/keys";
-import {handleFail} from "../helpers/helper";
+import { Navbar, Nav, MenuItem, NavItem, DropdownButton } from 'react-bootstrap';
 
 //TODO Clear styles
 
 const app = {
-    app: {display: 'flex', height: '100vh'},
+    app: {display: 'flex', height: '100vh', padding: '83px 0px 0px 0px'},
     currentRemote: {height: '100%', width: '70%'},
     dash: {height: '100%', width: '30%'},
     client: {height: '300px', borderBottom: '2px solid yellow'},
-    remote: {height: 'calc(100% - 300px)', overflowY: 'scroll'}
-
+    remote: {height: 'calc(100% - 300px)', overflowY: 'scroll'},
+    navbar: {height: '100%', width: '100%'}
 }
 
 class App extends React.Component {
@@ -25,10 +31,18 @@ class App extends React.Component {
         super(props)
         this.state = {
             battery: 0,
-            current: null
+            video_stats: {},
+            audio_stats: {},
+            current: null,
+            mixing: false,
+            hide: false
         }
-        // this.getCurrentId = this.getCurrentId.bind(this);
-        this.state = {update:false}
+        this.checkBattery = this.checkBattery.bind(this);
+        this.startMixing = this.startMixing.bind(this)
+        this.pauseMixing = this.pauseMixing.bind(this)
+        this.stopMixing = this.stopMixing.bind(this)
+        this.resumeMixing = this.resumeMixing.bind(this)
+        this.getNavigationBar = this.getNavigationBar.bind(this)
     }
 
     componentDidMount() {
@@ -48,10 +62,12 @@ class App extends React.Component {
 
             setInterval(() => {
                 client.getLocalVideoStats(stats => {
+                    this.setState({video_stats: stats})
                     console.log('Local Video Stats:\n', stats, '\nEnding the video stats')
                 })
 
                 client.getLocalAudioStats(stats => {
+                    this.setState({audio_stats: stats})
                     console.log('Local Audio Stats:\n', stats, '\nEnding the audio stats')
                 })
 
@@ -68,6 +84,30 @@ class App extends React.Component {
         });
     }
 
+    startMixing() {
+        const options = {
+            filePath: audioFile,
+            playTime: 0,
+            replace: false
+        }
+        window.localStream.startAudioMixing(options, (err) => {
+            if (err === null) {
+                this.setState({mixing: true})
+            }
+        })
+    }
+
+    pauseMixing() {
+        window.localStream.pauseAudioMixing()
+    }
+
+    resumeMixing() {
+        window.localStream.resumeAudioMixing()
+    }
+
+    stopMixing() {
+        window.localStream.stopAudioMixing()
+    }
 
     // getCurrentId(video, streamIds) {
     //     window.stream.stop()
@@ -83,16 +123,94 @@ class App extends React.Component {
     //
     // }
 
+    checkBattery(value) {
+        const batt = value
+        var status;
+        if (batt.battery < 10) {
+            status = zero
+        } else if (batt.battery < 25) {
+            status = tfive
+        } else if (batt.battery < 50) {
+            status = fifty
+        } else if (batt.battery < 75) {
+            status = sfive
+        } else status = cent
+        return status 
+    }
+
+    getNavigationBar () {
+        return <div>
+            <Navbar fixedTop>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <p data-tip={this.state.battery ? 
+                        `Current Battery Level: ${this.state.battery}`
+                        : 'Hang on tight, digging it out!' } href='#' 
+                    >
+                        <img src = 
+                        {this.checkBattery(this.state)}
+                        alt="Battery Level Indicator"
+                        />
+                    </p>
+                    
+                    <ReactTooltip place="top" type="dark" effect="float"/>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav pullRight>
+                    {/* TODO Channel Name */}
+                    {/* <NavItem eventKey={1}>
+                        <p data-tip="Channel Name">
+                            {window.location.search.split('=')[1]}
+                        
+                            <ReactTooltip place="top" type="dark" effect="float"/>
+                        </p>
+                    </NavItem> */}
+                    <NavItem>
+                        <DropdownButton 
+                            eventKey={3} 
+                            onClick = 
+                                {() => this.setState({hide: !this.state.hide})}
+                            bsSize="xsmall"
+                            title={'Audio Mixing Controls'} 
+                            noCaret
+                            id="music-dropdown"
+                        >
+                            <MenuItem eventKey={3.1} onClick={this.startMixing}>
+                                Start
+                            </MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.2} onClick={this.resumeMixing}>
+                                Play
+                            </MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3} onClick={this.pauseMixing}>
+                                Pause
+                            </MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3} onClick={this.stopMixing}>
+                                Stop
+                            </MenuItem>
+                        </DropdownButton>
+                    </NavItem>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+        </div>
+    }
+
     render() {
 
         return (
-            <div style={app.app}>
-                <div style={app.currentRemote}>
-                    <div id={'current'}/>
+        
+            <div>
+                <div>
+                {this.getNavigationBar()}
                 </div>
-                <div style={app.dash}>
-                    <div style={app.client}>
-                        <ClientVideoFeed battery={this.state.battery}/>
+                <div style={app.app}>
+                    <div style={app.currentRemote}>
+                        <div id={'current'}/>
                     </div>
                     <div style={app.remote}>
                         <OthersVideoFeed feeds={this.state.feeds}/>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {withRouter} from 'react-router-dom'
 import styled from 'styled-components'
+import request from 'request'
 
 const LandingPageWrapper = styled.div`
   display: flex;
@@ -23,10 +24,74 @@ class LandingPage extends React.Component {
         this.setState({value: event.target.value})
     }
 
-    handleSubmit(event){
-        // window.location = `/app?channel=${this.state.value}`
-        this.props.history.push(`/app?room=${this.state.value}`)
-        event.preventDefault()
+    componentDidMount() {
+        console.log("mounted!")
+		let client = window.agora = window.AgoraRTC.createClient({
+			mode: 'live',
+			codec: 'h264'
+		})
+	}
+
+	handleSubmit(event){
+        // event.preventDefault()
+        // const gCaptcha = event.target[1].value;
+        // console.log(gCaptcha)
+        // var options = {
+        //     uri: 'https://localhost:8081/sendChannel',
+        //     form: {
+        //        response: gCaptcha
+        //     }
+        // };
+      
+        // request.post(options, (err, response, body) => {
+        //     if (err){
+        //         console.log("Error: ", err);
+        //     }else{
+        //         var values = JSON.parse(body);
+        //         console.log("values: ", values);
+        //         if(values['success'] !== true){
+        //         console.log('Error in processing captcha! Entry Denied.');
+        //         }else{
+                    this.props.history.push(`/app?room=${this.state.value}`)
+        //         }
+        //     }
+        // });
+        // const body = {
+        //     secret: '6LdSmH8UAAAAAH7Gcm5hDTWD2dqLVR95WEVqoS75',
+        //     response: gCaptcha
+        // }
+        // axios.post('https://www.google.com/recaptcha/api/siteverify', {body})
+        // .then(result => {
+        //     console.log(result);
+        // })
+        // .then(data => console.log("data is ", data))
+        // .catch(error => console.log("Error: ", error));
+        // fetch('https://www.google.com/recaptcha/api/siteverify', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: {
+        //       secret: '6LdSmH8UAAAAAH7Gcm5hDTWD2dqLVR95WEVqoS75',
+        //       response: gCaptcha
+        //     }
+        //   })
+        //   .then(response => response.json())
+        //   .then(data => {
+        //         console.log(data);
+        //         if(data['success'] !== true){
+        //             console.log('Error in processing captcha! Entry Denied.');
+        //             // this.props.history.push('../404.html')
+        //         }else{
+        //             this.props.history.push(`/app?room=${this.state.value}`)
+        //         }
+        //   })
+        //   .catch(error => {
+        //       console.log('Error: ', error);
+        //   })
+        
+        
     }
 
 
@@ -36,7 +101,9 @@ class LandingPage extends React.Component {
                 <form onSubmit={this.handleSubmit} style={{alignSelf: 'center',padding: '30px'}}>
                     <label>
                         <p style={{marginBottom:15, color:'white'}}>Channel Name:</p>
-                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        <input type="text" value={this.state.value} onChange={this.handleChange} required/>
+                        {/* <div class="g-recaptcha" data-theme="dark" data-sitekey="6LdSmH8UAAAAAIeebu--oN0YSQlM-Z7MvvbuHX2b"> */}
+                        {/* </div> */}
                     </label>
                     <input type="submit" className={'button-primary'} value="Submit" style={{color:'white'}}/>
                 </form>
